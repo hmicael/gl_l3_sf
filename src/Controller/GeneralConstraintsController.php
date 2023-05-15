b5519b47dde3e17b3cc1447a51eced58e0fde829
henintsoa <handriamahadimby@gmail.com>
2023-05-15 17:55:01 +0200

    design general constraint & navbar

--- src/Controller/GeneralConstraintsController.php
+++ src/Controller/GeneralConstraintsController.php
@@ -16,41 +16,15 @@ class GeneralConstraintsController extends AbstractController
     #[Route('/', name: 'app_general_constraints_index', methods: ['GET'])]
     public function index(GeneralConstraintsRepository $generalConstraintsRepository): Response
     {
-        return $this->render('general_constraints/index.html.twig', [
-            'general_constraints' => $generalConstraintsRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/new', name: 'app_general_constraints_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, GeneralConstraintsRepository $generalConstraintsRepository): Response
-    {
-        $generalConstraint = new GeneralConstraints();
-        $form = $this->createForm(GeneralConstraintsType::class, $generalConstraint);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $generalConstraintsRepository->save($generalConstraint, true);
-
-            return $this->redirectToRoute('app_general_constraints_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('general_constraints/new.html.twig', [
-            'general_constraint' => $generalConstraint,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_general_constraints_show', methods: ['GET'])]
-    public function show(GeneralConstraints $generalConstraint): Response
-    {
         return $this->render('general_constraints/show.html.twig', [
-            'general_constraint' => $generalConstraint,
+            'general_constraints' => $generalConstraintsRepository->find(1)
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_general_constraints_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, GeneralConstraints $generalConstraint, GeneralConstraintsRepository $generalConstraintsRepository): Response
+    #[Route('/edit', name: 'app_general_constraints_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, GeneralConstraintsRepository $generalConstraintsRepository): Response
     {
+        $generalConstraint = $generalConstraintsRepository->find(1);
         $form = $this->createForm(GeneralConstraintsType::class, $generalConstraint);
         $form->handleRequest($request);
 
@@ -64,15 +38,5 @@ class GeneralConstraintsController extends AbstractController
             'general_constraint' => $generalConstraint,
             'form' => $form,
         ]);
-    }
-
-    #[Route('/{id}', name: 'app_general_constraints_delete', methods: ['POST'])]
-    public function delete(Request $request, GeneralConstraints $generalConstraint, GeneralConstraintsRepository $generalConstraintsRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$generalConstraint->getId(), $request->request->get('_token'))) {
-            $generalConstraintsRepository->remove($generalConstraint, true);
-        }
-
-        return $this->redirectToRoute('app_general_constraints_index', [], Response::HTTP_SEE_OTHER);
     }
 }
